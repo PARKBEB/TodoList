c03f4b202fa9c8e7f641146d83b9ae666c51b01d
PARKBEB <it.parkbeb@gmail.com>
2024-03-06 02:59:20 +0900

    json-post, get

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let dim = document.querySelector('.dim');
 
 let categoryIcons = document.querySelectorAll('.category_icon');
 let categoryModalNameText = document.querySelector('.category_modal_Name_text');
- 
+let categoryDel = document.querySelectorAll('.category_del');
+
 // dim이 뭐가 문제인지 모르겠음
 function categoryBtn() {
     categoryModal.style.display = categoryModal.style.display === "none" ? "block" : "none";
@@ -32,24 +33,56 @@ categoryIcons.forEach(function(data) {
 // 카테고리 Title 선택
 function categoryOkBtn() {
     let title = categoryModalNameText.value;
-    categoryModalNameText.value = "";
+    let categoryItem = document.querySelectorAll('.category_item'); // 변수를 전역으로 쓰면 categoryItem.length이 계속 1로 표시됨 이유 생각해야함
 
-    const data = {
-        "todo": title,
-        "icon": target
-    }
-
-    fetch("http://localhost:3000/data", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "content-type": "application/json; charset=utf-8"
+    if(categoryItem.length < 5 && title !== "") {
+        const data = {
+            "title": title,
+            "icon": target.classList
         }
-    })
-    .then(response => response.json())
-    .then(json => console.log(json))
+        fetch("http://localhost:3030/data", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "content-type": "application/json; charset=utf-8"
+            }
+        })
+        .then(response => response.json())
+        .then(() => categoryGetData())
+        .catch(error => console.error('Error:', error));
+
+    } else if(categoryItem.length > 4) {
+        alert("4개 이상 입력 불가");
+    } else if(title === "") {
+        alert("타이틀 공백 불가");
+    }
 
     categoryModal.style.display = categoryModal.style.display === "none" ? "block" : "none";
     dim.style.display = categoryModal.style.display === "none" ? "none" : "block";
+}
+
+function categoryGetData(){
+    fetch("http://localhost:3030/data")
+    .then(response => response.json())
+    .then(json => {
+        const array = [];
+        for(const data of json) {
+            let getData=
+            `<div class="category_item">
+                <div class="${data.icon[0]} ${data.icon[1]}"></div>
+                <div class="category_name">${data.title}</div>
+                <div class="category_del" onclick="category_del()">x</div>
+            </div>
+             `
+            array.push(getData);
+        }
+        document.querySelector('.category_item_all').innerHTML = array.join("");
+    })
+}
+
+categoryGetData();
+
+function category_del() {
+    alert("test")
 }
 
