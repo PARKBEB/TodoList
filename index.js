6f8d5bd9bfea6cfd17306fdc377494066b52f5cf
PARKBEB <it.parkbeb@gmail.com>
2024-03-06 00:18:10 +0900

    commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function todoBtn() {
 
 let target;
 
-// 카테고리 추가
+// 카테고리 아이콘 선택
 categoryIcons.forEach(function(data) {
     data.addEventListener('click', function(event) {  
         categoryIcons.forEach(function(otherIcon) { 
@@ -28,4 +28,28 @@ categoryIcons.forEach(function(data) {
         target.style.border = "2px solid black";
     });
 });
+
+// 카테고리 Title 선택
+function categoryOkBtn() {
+    let title = categoryModalNameText.value;
+    categoryModalNameText.value = "";
+
+    const data = {
+        "todo": title,
+        "icon": target
+    }
+
+    fetch("http://localhost:3000/data", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "content-type": "application/json; charset=utf-8"
+        }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+    categoryModal.style.display = categoryModal.style.display === "none" ? "block" : "none";
+    dim.style.display = categoryModal.style.display === "none" ? "none" : "block";
+}
 
